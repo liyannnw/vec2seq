8932c7e8a6f274284c050fc38c4667db6d2749b3
Liyan Wang <59081332+liyannnw@users.noreply.github.com>
2022-03-07 04:03:19 +0900

    Delete reformat.py

--- reformat.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-for line in sys.stdin:
-    line = line.strip()
-    abc,d = line.split(" : x  =>  x = ")
-    ab,c = abc.split(" :: ")
-    a,b = ab.split(" : ")
-    print("\t".join([a,b,c,d]))
