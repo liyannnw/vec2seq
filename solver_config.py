1e20f0926bad9dd4f9edd0e9bc7165016dd527f2
Liyan Wang <59081332+liyannnw@users.noreply.github.com>
2022-03-07 08:16:31 +0900

    Update solver_config.py

--- solver_config.py
+++ solver_config.py
@@ -4,7 +4,7 @@ train_path = data_dir_path + "nlg.train.npz"
 valid_path = data_dir_path + "nlg.valid.npz"
 test_path = data_dir_path + "nlg.test.npz"
 
-model_save_path ="results/solver_concat.pt"
+model_save_path ="results/solver-concat.pt"
 
 
 compose_mode = "concat"
@@ -15,4 +15,4 @@ patience=50
 epochs=1500
 
 
-results_save_path="results/solver_concat_basicrnn_sl.txt"
+results_save_path="results/solver-concat_basicrnn_sl.txt"
