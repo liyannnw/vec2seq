0013248c578b9f920c2881512c679adda30a0278
Liyan Wang <59081332+liyannnw@users.noreply.github.com>
2022-03-07 08:25:05 +0900

    Update

--- solver_config.py
+++ solver_config.py
@@ -4,6 +4,7 @@ train_path = data_dir_path + "nlg.train.npz"
 valid_path = data_dir_path + "nlg.valid.npz"
 test_path = data_dir_path + "nlg.test.npz"
 
+
 model_save_path ="results/solver-concat.pt"
 
 
